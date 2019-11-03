c0742f619eaec4ce23fbb1bc5817c9f8c3d747b1
Pekka Lammi <pekka.lammi@aalto.fi>
2019-11-03 13:53:02 +0200

    prizefield changes when incrementing values

--- components/molecules/StoreItemRow.tsx
+++ components/molecules/StoreItemRow.tsx
@@ -1,14 +1,23 @@
-import { Header, Segment, Grid, Button } from "semantic-ui-react";
+import {
+  Header,
+  Segment,
+  Grid,
+  Button,
+  Input,
+  Select
+} from "semantic-ui-react";
 import { useState } from "react";
 
 const StoreItemRow = () => {
   const [open, setOpen] = useState(false);
+  const [amount, setAmount] = useState(1);
+  const [price] = useState(1.23);
   return (
     <>
       <Segment attached>
         <Grid>
           <Grid.Row>
-            <Grid.Column width={6}>
+            <Grid.Column width={4}>
               <Header as="h4" image>
                 <Header.Content>
                   Coca Cola
@@ -16,27 +25,60 @@ const StoreItemRow = () => {
                 </Header.Content>
               </Header>
             </Grid.Column>
-            <Grid.Column width={3}>1.00 €</Grid.Column>
-            <Grid.Column width={7}>
-              <div>
+            <Grid.Column width={3}>
+              <Button.Group>
                 <Button
-                  icon="ellipsis horizontal"
-                  onClick={() => setOpen(!open)}
-                />
-                <Button
-                  content="Osta"
-                  icon="shopping cart"
-                  labelPosition="right"
                   primary
-                />
-              </div>
+                  onClick={() => (amount > 0 ? setAmount(amount - 1) : null)}
+                >
+                  -
+                </Button>
+                <div className="ui button" onClick={() => setAmount(1)}>
+                  {amount}
+                </div>
+                <Button primary onClick={() => setAmount(amount + 1)}>
+                  +
+                </Button>
+              </Button.Group>
+            </Grid.Column>
+            <Grid.Column
+              width={5}
+              style={{
+                display: "flex",
+                justifyContent: "center",
+                lineHeight: "36px"
+              }}
+            >
+              {amount <= 1
+                ? price + "€"
+                : amount +
+                  " * " +
+                  price +
+                  " € = " +
+                  (price * amount).toFixed(2) +
+                  " €"}
+            </Grid.Column>
+            <Grid.Column
+              width={4}
+              style={{ display: "flex", justifyContent: "flex-end" }}
+            >
+              <Button
+                icon="ellipsis horizontal"
+                onClick={() => setOpen(!open)}
+              />
+              <Button
+                content="Osta"
+                icon="shopping cart"
+                labelPosition="right"
+                primary
+              />
             </Grid.Column>
           </Grid.Row>
         </Grid>
       </Segment>
       {open ? (
         <Segment attached secondary>
-          Buy custom amounts here
+          Ilmoita namusetädille että on loppu
         </Segment>
       ) : (
         <></>
