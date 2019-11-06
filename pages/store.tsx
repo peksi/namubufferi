c878218ea599864191904b34acd18887aab6f786
Pekka Lammi <pekka.lammi@aalto.fi>
2019-11-06 08:06:36 +0200

    bäkintosh

--- pages/store.tsx
+++ pages/store.tsx
@@ -1,7 +1,15 @@
-import { Menu, Container, Segment, Table, Header } from "semantic-ui-react";
+import {
+  Menu,
+  Container,
+  Segment,
+  Table,
+  Header,
+  Button
+} from "semantic-ui-react";
 import { jsx, css } from "@emotion/core";
 import styled from "@emotion/styled";
 import StoreItemRow from "../components/molecules/StoreItemRow";
+import Link from "next/link";
 
 const MarginFixer = styled.div`
   margin-bottom: -2px;
@@ -9,32 +17,37 @@ const MarginFixer = styled.div`
 
 const Store = () => {
   return (
-    <Container>
-      {/* <MarginFixer> */}
-      <Menu attached tabular widths={3}>
-        <Menu.Item as="a">Snäkkilaatikot</Menu.Item>
-        <Menu.Item active as="a">
-          Jääkaappi
-        </Menu.Item>
-        <Menu.Item as="a">Pakastin</Menu.Item>
-      </Menu>
-      {/* </MarginFixer> */}
-      <Segment attached>
-        <Header as="h4" attached="top" block>
-          Terveysruoka
-        </Header>
-        <StoreItemRow />
-        <StoreItemRow />
-        <StoreItemRow />
+    <>
+      <Container>
+        <Link href="/">
+          <Button danger>Takaisin</Button>
+        </Link>
+        {/* <MarginFixer> */}
+        <Menu attached tabular widths={3}>
+          <Menu.Item as="a">Snäkkilaatikot</Menu.Item>
+          <Menu.Item active as="a">
+            Jääkaappi
+          </Menu.Item>
+          <Menu.Item as="a">Pakastin</Menu.Item>
+        </Menu>
+        {/* </MarginFixer> */}
+        <Segment attached>
+          <Header as="h4" attached="top" block>
+            Terveysruoka
+          </Header>
+          <StoreItemRow />
+          <StoreItemRow />
+          <StoreItemRow />
 
-        <Header as="h4" attached="top" block>
-          Epäterveysruoka
-        </Header>
-        <StoreItemRow />
-        <StoreItemRow />
-        <StoreItemRow />
-      </Segment>
-    </Container>
+          <Header as="h4" attached="top" block>
+            Epäterveysruoka
+          </Header>
+          <StoreItemRow />
+          <StoreItemRow />
+          <StoreItemRow />
+        </Segment>
+      </Container>
+    </>
   );
 };
 
