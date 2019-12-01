3aff311abe4a08636036f16a152b7fb2f2da3cbf
Pekka Lammi <pekka.lammi@aalto.fi>
2019-12-01 19:19:20 +0200

    hide the api key from client side

--- apolloconfig.tsx
+++ apolloconfig.tsx
@@ -1,10 +1,12 @@
 import ApolloClient from "apollo-boost";
 import fetch from "isomorphic-fetch";
+import getConfig from "next/config";
+const { serverRuntimeConfig, publicRuntimeConfig } = getConfig();
 
 export const client = new ApolloClient({
   uri: "https://athene-namubufferi.herokuapp.com/v1/graphql",
   headers: {
-    "X-Hasura-Admin-Secret": process.env.HASURA_ADMIN_SECRET
+    "X-Hasura-Admin-Secret": serverRuntimeConfig.HASURA_ADMIN_SECRET
   },
   fetch
 });
