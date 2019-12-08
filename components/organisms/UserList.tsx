d4b4842eb42b1eb3534300a420bae7fea877df1f
Pekka Lammi <pekka.lammi@aalto.fi>
2019-12-08 18:31:32 +0200

    filter users

--- components/organisms/UserList.tsx
+++ components/organisms/UserList.tsx
@@ -5,15 +5,36 @@ import {
   Header,
   Segment,
   Message,
-  Icon
+  Icon,
+  Input
 } from "semantic-ui-react";
 import UserListRow from "../molecules/UserListRow";
 import useFetch from "use-http";
+import { useState, useEffect, useRef } from "react";
 
 const UserList = () => {
-  const { loading, error, data } = useFetch("/api/getUsers", { data: [] }, []);
+  // const { loading, error, data } = useFetch("/api/getUsers", { data: [] }, []);
+  const [request, response] = useFetch("/api");
+  const [users, setUsers] = useState<any>([]);
+  const [initialUsers, setInitialUsers] = useState<any>([]);
 
-  if (loading) {
+  // componentDidMount
+  const mounted = useRef(false);
+  useEffect(() => {
+    if (mounted.current) return;
+    mounted.current = true;
+    initializeUsers();
+  });
+
+  async function initializeUsers() {
+    const initialUsers = await request.get("/getUsers");
+    if (response.ok) {
+      setInitialUsers(initialUsers.data.user);
+      setUsers(initialUsers.data.user);
+    }
+  }
+
+  if (request.loading) {
     return (
       <Container>
         <Message icon>
@@ -26,11 +47,11 @@ const UserList = () => {
       </Container>
     );
   }
-  if (error) {
+  if (request.error) {
     return (
       <Container>
         <Message icon negative>
-          <Message.Header>Error: {error.message}</Message.Header>
+          <Message.Header>Error: {request.error.message}</Message.Header>
         </Message>
       </Container>
     );
@@ -42,13 +63,36 @@ const UserList = () => {
         <Header as="h4" attached="top" block>
           Valitse käyttäjä
         </Header>
-        {data.data.user
-          .sort((a, b) => {
-            return a.name.localeCompare(b.name);
-          })
-          .map(data => {
-            return <UserListRow key={data.uuid} data={data} />;
-          })}
+        <Segment>
+          <Input
+            icon="users"
+            iconPosition="left"
+            placeholder="Filtteröi kiltalaisista"
+            onChange={event => {
+              const filterString = event.target.value;
+
+              setUsers(
+                initialUsers.filter(
+                  user =>
+                    user.name
+                      .toLowerCase()
+                      .indexOf(filterString.toLowerCase()) > -1
+                )
+              );
+            }}
+          />
+        </Segment>
+        {users ? (
+          users
+            .sort((a, b) => {
+              return a.name.localeCompare(b.name);
+            })
+            .map(data => {
+              return <UserListRow key={data.uuid} data={data} />;
+            })
+        ) : (
+          <></>
+        )}
       </Segment.Group>
     </Container>
   );
