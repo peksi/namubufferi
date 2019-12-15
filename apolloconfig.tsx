ac51e4e8e5ca25740665ff99a0cab58654f0ee5e
Pekka Lammi <pekka.lammi@aalto.fi>
2019-12-15 16:53:42 +0200

    fix: environment keys

--- apolloconfig.tsx
+++ apolloconfig.tsx
@@ -3,18 +3,13 @@ import { createHttpLink } from "apollo-link-http";
 import { InMemoryCache } from "apollo-cache-inmemory";
 
 import fetch from "isomorphic-fetch";
-import getConfig from "next/config";
-const { serverRuntimeConfig, publicRuntimeConfig } = getConfig();
-
-console.log(serverRuntimeConfig.hasura_key);
-console.log(publicRuntimeConfig.api_url);
 
 export const client = new ApolloClient({
   ssrMode: true,
   link: createHttpLink({
     uri: "https://athene-namubufferi.herokuapp.com/v1/graphql",
     headers: {
-      "X-Hasura-Admin-Secret": serverRuntimeConfig.hasura_key
+      "X-Hasura-Admin-Secret": process.env.HASURA_ADMIN_SECRET
     },
     fetch: fetch
   }),
