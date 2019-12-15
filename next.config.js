baefd3de525fb5d725bc77e042aacaa3c05aa648
Pekka Lammi <pekka.lammi@aalto.fi>
2019-12-15 16:37:01 +0200

    change config for serverless

--- next.config.js
+++ next.config.js
@@ -4,12 +4,16 @@ const path = require("path");
 const Dotenv = require("dotenv-webpack");
 
 module.exports = {
-  serverRuntimeConfig: {
-    // Will only be available on the server side
-    hasura_key: process.env.HASURA_ADMIN_SECRET
-  },
-  publicRuntimeConfig: {
-    // Will be available on both server and client
+  env: {
+    hasura_key: process.env.HASURA_ADMIN_SECRET,
     api_url: process.env.API_URL
   }
+  // serverRuntimeConfig: {
+  //   // Will only be available on the server side
+  //   hasura_key: process.env.HASURA_ADMIN_SECRET
+  // },
+  // publicRuntimeConfig: {
+  //   // Will be available on both server and client
+  //   api_url: process.env.API_URL
+  // }
 };
