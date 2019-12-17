10bd85e11acec3c4f9a29dba02607d963b04e394
Pekka Lammi <pekka.lammi@aalto.fi>
2019-12-17 20:38:52 +0200

    remove commented-out stuff

--- next.config.js
+++ next.config.js
@@ -8,12 +8,4 @@ module.exports = {
     hasura_key: process.env.HASURA_ADMIN_SECRET,
     api_url: process.env.API_URL
   }
-  // serverRuntimeConfig: {
-  //   // Will only be available on the server side
-  //   hasura_key: process.env.HASURA_ADMIN_SECRET
-  // },
-  // publicRuntimeConfig: {
-  //   // Will be available on both server and client
-  //   api_url: process.env.API_URL
-  // }
 };
